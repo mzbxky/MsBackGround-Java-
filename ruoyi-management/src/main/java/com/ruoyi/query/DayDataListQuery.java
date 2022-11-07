88638bdb24af910158e9b851bde5728b3fa10a61
mzbxky <1376055937@qq.com>
2022-11-07 11:49:03 +0800

    山东码上后台管理系统(后端部分):表名修改

--- ruoyi-management/src/main/java/com/ruoyi/query/DayDataListQuery.java
+++ ruoyi-management/src/main/java/com/ruoyi/query/DayDataListQuery.java
@@ -1,16 +1,26 @@
 package com.ruoyi.query;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
+@ApiModel(description = "天级数据入参（码上数据库）")
 public class DayDataListQuery {
+    @ApiModelProperty(value = "分页参数",required = true)
     private Integer pageNum;
+    @ApiModelProperty(value = "分页参数",required = true)
     private Integer pageSize;
+    @ApiModelProperty(value = "媒体名称",required = false)
     private String mediaName;
-    private String placementName;
+    @ApiModelProperty(value = "开始时间",required = true)
     private String startDate;
+    @ApiModelProperty(value = "结束时间",required = true)
     private String endDate;
+    @ApiModelProperty(value = "开发者账号",required = false)
     private Boolean byDate;
+    @ApiModelProperty(value = "开发者账号",required = false)
     private Boolean byMediumName;
+    @ApiModelProperty(value = "开发者账号",required = false)
     private Boolean byPlacementName;
 }
